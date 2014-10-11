11f0dffebbd5eb4368d5d0846af7dcf8e5790d19
Ivan Sadovsky <lovelas123123@gmail.com>
2014-10-11 13:04:55 +0300

    BarrelN2
    blablabla

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package com.kademika;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		String mtQuote = "Let us always meet each other with smile, for the smile is the beggining of love.";
+				
+		
+		System.out.println(mtQuote.replace("smile", ":)"));
+
+	}
+
+}
