a1e6b9efe0bbcf9bc1d5473217e590cfc6316c38
Ivan Sadovsky <lovelas123123@gmail.com>
2014-10-13 23:22:06 +0300

    Day3

---
+++ day3/TEST.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class TEST {
+
+	public static void main(String[] args) {
+		
+
+		int[] data = {10, -3, 25, 4};
+		
+		System.out.println(data.length);
+		
+			}		
+		
+
+}
+			
+	
+		
--- day3/b6LastElementsOfArrays.java
+++ day3/b6LastElementsOfArrays.java
@@ -3,16 +3,28 @@ package day3;
 public class b6LastElementsOfArrays {
 
 	public static void main(String[] args) {
+		
+	int[] data1 = {1, 10, 5, 7, 6};
+	int[] data2 = {0};
+	int[] data3 = {};
+	int[] data4 = {0, 10, 15, 25, 16};
+	int[] data5 = {- 3, 25, 2};
 	
-		int[]data = {1, 10, 5, 7, 6};
-		int[]data1 = {0};
-		int[]data2 = {};
-		int[]data3 = {0, 0, 15, 25, 16};
-		int[]data4 = {-3, 25, 2};
-	}
+	System.out.println(getLast(data1));
+	System.out.println(getLast(data2));
+	System.out.println(getLast(data3));
+	System.out.println(getLast(data4));
+	System.out.println(getLast(data5));
 	
-	if ((int[]data == 0) || (int[]data1 == 0) || (int[]data2 == 0)) {
-		return
+		}		
+	
+	static int getLast(int[] data){
+		if (data.length > 0) {
+			return data[data.length - 1];
+		}
+		return -1;
 	}
+		
 }
+
 
