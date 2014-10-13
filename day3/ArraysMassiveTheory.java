b0f7a03af8129fdc4332e2ea9f7e3fd238e425e0
isadov <lovelas123123@gmail.com>
2014-10-13 19:56:29 +0300

    Day3

---
+++ day3/ArraysMassiveTheory.java
@@ -0,0 +1,16 @@
+package day3;
+
+public class ArraysMassiveTheory {
+
+	public static void main(String[] args) {
+		
+// <type [] <name> = new<type>[<count>]; count = кол-во элементов
+// int [] nums=new int [20];
+// String [] names = new String [10];
+// String [] colors = new String [] {"red", "blue", "green"};
+// String [] colors = {"red", "blue", "green"};
+// Строка 10 == 11 !!!
+		
+	}
+
+}
---
+++ day3/b6LastElementsOfArrays.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class b6LastElementsOfArrays {
+
+	public static void main(String[] args) {
+	
+		int[]data = {1, 10, 5, 7, 6};
+		int[]data1 = {0};
+		int[]data2 = {};
+		int[]data3 = {0, 0, 15, 25, 16};
+		int[]data4 = {-3, 25, 2};
+	}
+	
+	if ((int[]data == 0) || (int[]data1 == 0) || (int[]data2 == 0)) {
+		return
+	}
+}
+
