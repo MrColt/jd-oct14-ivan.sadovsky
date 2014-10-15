eb18f0aad76fd6ac2f63cc2430c8f38a84f15019
isadov <lovelas123123@gmail.com>
2014-10-15 11:33:13 +0300

    Revert "Day3"
    This reverts commit 458d87ea0c2a8899b4e1d3e714307b226e355aa4.

---
+++ day3/TEST.java
@@ -0,0 +1,15 @@
+package day3;
+import java.util.*;
+public class TEST {
+
+	public static void main(String[] args) {
+		 
+		        int[] a = new int[10]; 
+		        Random rnd = new Random(); 
+		        for (int c = 0; c < a.length; ++c) a[c] = rnd.nextInt(21) - 10; 
+		        for (int c = 0; c < a.length; ++c) System.out.print(a[c] + " "); 
+		    } 
+		
+	}
+
+
