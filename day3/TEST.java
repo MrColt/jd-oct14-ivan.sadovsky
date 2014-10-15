806ec55192d0652414fb484ade70999acf10f5e9
Ivan Sadovsky <lovelas123123@gmail.com>
2014-10-15 23:53:12 +0300

    day3

--- day3/TEST.java
+++ day3/TEST.java
@@ -4,10 +4,9 @@ public class TEST {
 
 	public static void main(String[] args) {
 		 
-		        int[] a = new int[10]; 
-		        Random rnd = new Random(); 
-		        for (int c = 0; c < a.length; ++c) a[c] = rnd.nextInt(21) - 10; 
-		        for (int c = 0; c < a.length; ++c) System.out.print(a[c] + " "); 
+		      
+		
+		
 		    } 
 		
 	}
